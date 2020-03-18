e06a01c5dd6a1666d50389bd37165ddd9ed5f550
Erica Sousa <ericams175@gmail.com>
2020-03-18 08:40:38 -0300

    Organizando .2

--- Exercicios/introducaoJs/src/components/Vingadores.jsx
+++ Exercicios/introducaoJs/src/components/Vingadores.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const CapitaoAmerica = props => //'CapitaoAmerica' (componente seta) é uma variável que recebe uma função
-    <h4>Olá {props.nome}, eu sou o Capitão América!</h4>
+    <h5>Olá {props.nome}, eu sou o Capitão América!</h5>
 
 const ViuvaNegra = props => 
-    <h4>Olá {props.nome}, eu sou a Viúva Negra!</h4>
+    <h5>Olá {props.nome}, eu sou a Viúva Negra!</h5>
 
 export{CapitaoAmerica,ViuvaNegra}
--- Exercicios/introducaoJs/src/components/pagInicial.jsx
+++ Exercicios/introducaoJs/src/components/pagInicial.jsx
@@ -7,58 +7,53 @@ import {CapitaoAmerica,ViuvaNegra} from './Vingadores'
 
 export default ()=>
     <>
-      <div class="cabecalho">
-        <h1 class="titulo">Desenvolvimento de Software para WEB</h1>
-      </div>
+        <div class="cabecalho">
+            <h1 class="titulo">Desenvolvimento de Software para WEB - 2020.1</h1>
+        </div>
 
-      <div class="grid1">
-        <ul>
-          <li><h1>Introdução ao React</h1></li>
-            <ul>
-                
-                <ul>
-                    <li><h3><a class="active">Aula 01: Uso do JSX</a></h3></li>
-                    <ul>
-                        <li>1-) Superman</li>
-                        <li>2-) Batman</li>
-                        <li>3-) Wonder Woman</li>
-                    </ul>
-                </ul>
+        <div class="grid1">
+            <h1>Introdução ao React</h1>
+            
+            <div class="aula01">
+                <h3><li>Aula 01: Uso do JSX</li></h3>
+                <ul class="ex01">
+                    <li>1-) Superman</li>
+                    <li>2-) Batman</li>
+                    <li>3-) Wonder Woman</li>
+                 </ul>
+            </div>
 
-                <ul>
-                    <li><h3><a class="active">Aula 02: Props</a></h3></li>
+            <div class="aula02">
+                <h3><li>Aula 02: Props</li></h3>
+                <ul class="ex02">
                     <Estudante nome='Érica Sousa' idade={21} curso='Sistemas de informação' cidadeNatal='Boa Viagem'/>
                 </ul>
+            </div>
 
-                <ul>
-                    <li><h3><a class="active">Aula 03: Componentes</a></h3></li>
-                    <h4>Relembrando...</h4>
-                    <ul>
-                        <h4>Professor</h4>
+            <div class="aula03">
+                <h3><li>Aula 03: Componentes</li></h3>
+                <h4>Relembrando...</h4>
+                    <div class="ex03-01">
+                        <h5>Professor</h5>
                         <Professor nome = 'Wladimir' area = 'Matemática' nivel = 'Doutorado' curso = 'Ciência da Computação'/>  
-                        <h4>Estudante</h4>
+                        <h5>Estudante</h5>
                         <Estudante  nome='Letícia Farias' idade={20} curso='Sistemas de informação' cidadeNatal='Quixeramobim'/>  
-                    </ul>
+                    </div>
 
-                    <ul>
-                        <li> <h3>Componentes - Múltiplos</h3> </li>
-                        <ul>
-                            <li><h4>Exemplo:</h4></li>
+                    <div>
+                        <h3><li>Componentes - Múltiplos</li></h3>
+                        <div class="ex03-02">
                             <CapitaoAmerica nome = 'Érica'/>
                             <ViuvaNegra nome = 'Érica'/>
-                        </ul>
-                    </ul>
+                        </div>
+                    </div>
 
-                    <ul>
-                        <li><h3>Componentes - Relação HardCoded</h3></li>
-                        <ul>
-                            <li><h4>Exemplo:</h4></li>
-                            <Casa/>
-                        </ul>
-                    </ul>
-                </ul>        
-            </ul>
-        </ul>
-      </div>
+                    <div>
+                        <h3><li>Componentes - Relação HardCoded</li></h3>
+                        <div class="ex03-02"><Casa/></div>
+                    </div>
+            </div>
+
+        </div>
     </>
     
